fe29e566968434d944b7b27589c863529910e324
MohamedBadrr <badr59497@gmail.com>
2024-06-10 23:11:36 +0300

    project

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import "./App.css";
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [language, setLanguage] = useState("en");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showForm , setShowForm] = useState(true);
+
+  useEffect((e) => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks"))||[];
+    setTasks(savedTasks);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+
+  const toggleTask = (index) => {
+    const newTasks = tasks.map((task, i) => {
+      if (i === index) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  };
+
+  const toggleLanguage = () => {
+    setLanguage(language === "en" ? "ar" : "en");
+  };
+
+  const filteredTasks = tasks.filter((task) =>
+    task.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <>
+      
+    <div className={` ${language === "ar" ? "rtl" : "ltr"} screen`}>
+      <div className="home-form">
+        <div className="search-section">
+          <Search
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            language={language}
+          />
+          <button className=" text-center" onClick={()=>{setShowForm(true)}}>
+            {language === "en" ? "+ Add Task" : "أضف مهمة + "}
+            
+          </button>
+          <span>EN</span>
+          <div class="form-check form-switch">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault"
+              onClick={toggleLanguage}
+            />
+          </div>
+          <span>AR</span>
+        </div>
+        <TaskList
+          tasks={filteredTasks}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+          language={language}
+        />
+        {showForm && <TaskForm addTask={addTask} language={language} showform={setShowForm}/>}
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default App;
+
+
+const Search = ({ searchTerm, setSearchTerm, language }) => {
+  return (
+    <input
+      type="text" 
+      placeholder={language === 'en' ? 'Search Tasks' : 'ابحث في المهام'}
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      className="input-search"
+    />
+  );
+};
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const TaskList = ({ tasks, deleteTask, toggleTask, language }) => {
+  return (
+    <ul className="">
+      {tasks.map((task, index) => (
+        <li key={index} className="flex justify-between items-top border p-2 mt-3 m-auto one-task">
+          <div>
+            <p>Task #{index+1}</p>
+            <p className={task.completed ? 'line-through' : ''}>{task.name}</p>
+            <p>{task.time}</p>
+          </div>
+          <div>
+            <button
+              onClick={() => toggleTask(index)}
+              className="text-success p-2 mr-2"
+            >
+              {task.completed
+                ? language === 'en'
+                  ? 'Undo'
+                  : 'تراجع'
+                : language === 'en'
+                ? 'Complete'
+                : 'إنهاء'}
+            </button>
+            <button
+              onClick={() => deleteTask(index)}
+              className="text-danger p-2"
+            >
+              {language === 'en' ? 'Delete' : 'حذف'}
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TaskList;
